2ba81d6223caffe1f3f90230fce10788a0ee47eb
gautam2907 <gautambhatiani.1999@gmail.com>
2020-10-06 16:45:17 +0530

    slot booking end points are added

--- src/main/java/com/example/dailyappointmentservice/controller/AppointmentController.java
+++ src/main/java/com/example/dailyappointmentservice/controller/AppointmentController.java
@@ -2,19 +2,55 @@ package com.example.dailyappointmentservice.controller;
 
 
 import com.example.dailyappointmentservice.model.Appointment;
+import com.example.dailyappointmentservice.model.Availability;
 import com.example.dailyappointmentservice.model.GenericResponse;
+import com.example.dailyappointmentservice.model.Physician;
 import com.example.dailyappointmentservice.repository.AppointmentRepository;
+import com.example.dailyappointmentservice.repository.AvailablityRepository;
+import com.example.dailyappointmentservice.repository.PhysicianRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/appointment")
 public class AppointmentController {
 
     @Autowired
     private AppointmentRepository repository;
+    
+    @Autowired
+    private AvailablityRepository availRepository;
+    
+    @Autowired
+    private PhysicianRepository	physicianRepository;
+    
+    
+    @RequestMapping(value="/get-physicians/{speciality}")
+    public ResponseEntity<List<Physician>> getPhysicians(@PathVariable String speciality) {
+    	List<Physician> physiciansWithSpeciality = physicianRepository.findBySpeciality(speciality);
+    	return new ResponseEntity<List<Physician>>(physiciansWithSpeciality, HttpStatus.OK);
+    }
+    
+    
+    @RequestMapping(value="/get-physicians-all")
+    public ResponseEntity<List<Physician>> getAllPhysicians() {
+    	List<Physician> physicians = physicianRepository.findAll();
+    	return new ResponseEntity<List<Physician>>(physicians, HttpStatus.OK);
+
+    }
+    
+    @RequestMapping(value="/get-slots/{physicianId}/{date}")
+    public ResponseEntity<List<Availability>> getAvailableSlots(@PathVariable String physicianId, @PathVariable Date date) {
+    	List<Availability> allAvailSlots = availRepository.findByPhysicianIdAndDate(physicianId, date);
+    	return new ResponseEntity<List<Availability>>(allAvailSlots, HttpStatus.OK);
+    }
 
     @RequestMapping(value = "/set-appointment", method = RequestMethod.POST)
     public GenericResponse setAppointment(@RequestBody Appointment appointment){
